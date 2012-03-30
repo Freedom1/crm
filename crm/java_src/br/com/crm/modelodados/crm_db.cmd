3eac10256fc95131d22c53631fcbc8f42bcd3fa5
silvanopessoa <silvanopessoa@gmail.com>
2012-03-29 21:40:37 -0300

    Tabela Serie e Tipo de Ensino

--- crm/java_src/br/com/crm/modelodados/crm_db.cmd
+++ crm/java_src/br/com/crm/modelodados/crm_db.cmd
@@ -1,4 +1,5 @@
 sqlplus / as sysdba @ddl.crm.sql
 sqlplus / as sysdba @ddl.documento.sql
+sqlplus / as sysdba @ddl.serie.sql
 sqlplus / as sysdba @ddl.domicilio.sql
 sqlplus / as sysdba @dml.crm.sql
---
+++ crm/java_src/br/com/crm/modelodados/ddl.serie.sql
@@ -0,0 +1,24 @@
+spool ddl.serie.txt
+
+connect crm/crm
+
+--------------------------------------------------------------------------------
+--------------------------- Data Definition Language ---------------------------
+-------------------------------------- DDL -------------------------------------
+------------------------------ CREATE, ALTER, DROP -----------------------------
+---------------------------------- GRUPO SERIE ---------------------------------
+--------------------------------------------------------------------------------
+
+create table tb_serie(
+    id_serie                    number(18,0)		not null         constraint pk_serie primary key,
+    tx_serie                    varchar2(200)     	not null,--2° ano/3° ano/9° ano
+    id_tipo_ensino              number(18,0)     	not null--fk
+);
+
+create table tb_tipo_ensino(
+    id_tipo_ensino                number(18,0)    	not null         constraint pk_tipo_ensino primary key,
+    tx_tipo_ensino                varchar2(200)     not null--Educação infantil/Ensino fundamental/Ensino médio/
+);
+
+spool off
+exit
